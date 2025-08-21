9f16d1f8b9eeaa4121b9a20ea3b7b2ad88f7ee8c
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-08-21 06:31:15 +0000

    Updating to latest FlutterFlow output.

--- lib/custom_code/my_stream_service.dart
+++ lib/custom_code/my_stream_service.dart
@@ -1,21 +1,33 @@
 import 'dart:async';
+import 'package:dchs_flutter_beacon/dchs_flutter_beacon.dart';
+import '/flutter_flow/flutter_flow_util.dart';
 
 class MyStreamService {
   static final MyStreamService _instance = MyStreamService._internal();
   factory MyStreamService() => _instance;
   MyStreamService._internal();
 
-  StreamSubscription? _subscription;
+  StreamSubscription<RangingResult>? _streamRanging;
 
-  void startListening(Stream<String> myStream) {
-    _subscription ??= myStream.listen((data) {
-      print("Received: $data");
+  void startListening(Stream<RangingResult> myStream) {
+    _streamRanging ??= myStream.listen((result) async {
+      print('in ranging.listen');
+      if (result.beacons.isNotEmpty) {
+        result.beacons.sort((a, b) => a.accuracy.compareTo(b.accuracy));
+        final nearest = result.beacons;
+        FFAppState().beaconDistanceList =
+            nearest.map((e) => e.accuracy.toStringAsFixed(2)).toList();
+        FFAppState().beaconIdList =
+            nearest.map((e) => e.proximityUUID).toList();
+        // FFAppState().beaconNameList = nearest.map((e) => e.macAddress).toList();
+        //safeSetState((){});
+      }
       // handle data
     });
   }
 
   void stopListening() {
-    _subscription?.cancel();
-    _subscription = null;
+    _streamRanging?.cancel();
+    _streamRanging = null;
   }
 }
